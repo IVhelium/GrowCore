68bbdb278a566470813fac82062e6f737263e6bb
IVhelium <hlebusektv780@gmail.com>
2026-03-05 23:47:59 +0000

    BEM class rename

--- templates/assets/js/components/nav.js
+++ templates/assets/js/components/nav.js
@@ -3,8 +3,8 @@ const navContainer = document.querySelector('.nav');
 
 window.addEventListener('scroll', () => {       
     if (window.scrollY > 50) {                  // If scrolled more than 50px
-        navContainer.classList.add('scrolled');
+        navContainer.classList.add("nav--scrolled");
     } else {
-        navContainer.classList.remove('scrolled');
+        navContainer.classList.remove("nav--scrolled");
     }
 });
