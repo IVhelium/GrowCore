7df5133b34df9471eb953240ab1b2759f414c2aa
IVhelium <hlebusektv780@gmail.com>
2026-05-12 23:24:18 +0100

    settings update

--- src/core/config.py
+++ src/core/config.py
@@ -1,17 +1,24 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# Database URL create
+
 class Settings(BaseSettings):
+    # Database
     DB_USER: str
     DB_PASS: str
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
     
+    # JWT
+    JWT_SECRET: str
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    
+    
     @property
     def DATABASE_URL_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
